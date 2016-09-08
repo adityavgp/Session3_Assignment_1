4249ae61bbaa0f5f17907ea252c740a27829008d
adityavgp <gpaditya11@gmail.com>
2016-09-08 19:47:50 +0530

    Add files via upload
    answer is c and the code is modified as attached

---
+++ A3_1.java
@@ -0,0 +1,11 @@
+package com.a3.classes;
+
+public class A3_1{
+public static void main(String[] args){
+A3_1.print(10);
+}
+static void print (int x) {
+System.out.println (x);
+System.exit(0);
+}
+}
